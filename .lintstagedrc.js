6546b51fb0f678c0057fe62f4e9e8bb2964c4bf3
loxygenK <me@loxygen.dev>
2022-07-01 10:37:31 +0900

    🐛 use correct joining for `--file` flags of `next lint` command

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 function nextFix(/** @type string[] */ files) {
-  const fileFlags = files.map((f) => `--file ${f}`);
-  return `next lint --fix ${fileFlags}`;
+  const fileFlags = files.map((f) => `--file ${f} `);
+  return `next lint --fix ${fileFlags.join(" ")}`;
 }
 
 module.exports = {
