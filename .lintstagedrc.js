81f95f9d6d7910fd832c83ed6ffdd489a8c1b336
loxygenK <me@loxygen.dev>
2022-07-01 10:50:10 +0900

    🛠 Adopt some configuration to use newer scripts

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,10 @@
-function nextFix(/** @type string[] */ files) {
+function runFix(/** @type string[] */ files) {
   const fileFlags = files.map((f) => `--file ${f} `);
-  return `next lint --fix ${fileFlags.join(" ")}`;
+  return `pnpm fix ${fileFlags.join(" ")}`;
 }
 
 module.exports = {
   "./**/*.ts?(x)": [
-    nextFix, "prettier -cw "
+    runFix, "pnpm fix:prettier"
   ]
 }
